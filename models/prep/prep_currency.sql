7f8503e8297268598a1e3129a937d175eb3ca21c
Damilare <sunday.damilare@lmu.edu.ng>
2025-08-27 14:39:33 +0100

    fixing boolean

--- models/prep/prep_currency.sql
+++ models/prep/prep_currency.sql
@@ -11,5 +11,5 @@ SELECT
 FROM 
     {{ ref('base_open_exchange__open_exchange_currency_raw') }} AS currency
 WHERE 
-    LOWER(_fivetran_deleted) = "false"
+    _fivetran_deleted = FALSE
 QUALIFY ROW_NUMBER() OVER (PARTITION BY code ORDER BY _fivetran_synced DESC) = 1
