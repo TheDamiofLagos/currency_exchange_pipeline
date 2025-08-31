19ff3eb686d337061a6ae34bf5fb0f4135f86ea0
Damilare Abolaji <damialerts@gmail.com>
2025-08-31 15:48:16 +0000

    finished prep models

---
+++ models/prep/prep_rates.sql
@@ -0,0 +1,18 @@
+{{
+  config(
+    materialized = 'table'
+    )
+}}
+
+SELECT
+    currency_code AS tocurrency,
+    data_base AS fromcurrency,
+    data_timestamps AS created_at,
+    rate
+FROM
+    {{ ref('base_open_exchange__open_exchange_rates_raw') }}
+WHERE
+    _fivetran_deleted = FALSE
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY currency_code ORDER BY DATE(data_timestamps) DESC
+) = 1
