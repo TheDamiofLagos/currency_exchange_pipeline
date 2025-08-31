1bbc57c3b091bda38730ab877eb4c41757185812
Damilare Abolaji <damialerts@gmail.com>
2025-08-31 17:34:49 +0000

    built fact tables

---
+++ models/dimensional/fact_rates.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized = 'incremental'
+    )
+}}
+
+SELECT
+    rates.tocurrency,
+    currency.currency_name,
+    rates.fromcurrency,
+    rates.created_at,
+    rates.rate
+FROM 
+    {{ ref('prep_rates')}} AS rates
+LEFT JOIN
+    {{ ref("prep_currency")}} AS currency
+ON
+    rates.tocurrency = currency.currency_code
+WHERE
+    1 = 1
+    {% if is_incremental() %}
+    AND rates.created_at > (SELECT MAX(created_at) FROM {this})
+    {% endif %}
